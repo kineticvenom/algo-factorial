8e45d9dc0d6da2b4546cca33c30c000d85d8ed84
kineticvenom <kalebvarnes@yahoo.com>
2022-05-25 12:59:57 -1000

    New Code

--- js/factorial.js
+++ js/factorial.js
@@ -1,2 +1,9 @@
 exports.factorial = function(num) {
-};
+
+  let answer= 1
+  for (let i=1; i<=num; i++ ){
+    answer *= i;
+  }
+return answer;
+  }
+  
--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,11 @@
+
 def factorial(num):
-	# your code here
+	answer = 1
+	i = 0
+	for i in range(1,num +1) :
+		answer = answer * i
+		i+1
+		
+  	
+	return answer
+  
